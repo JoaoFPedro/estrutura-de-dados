f772fd9bbfc0c344839706b922eaeacd9cf74b9a
Joao Pedro <joao.fpsilva98@gmail.com>
2024-07-10 20:31:58 -0300

    arrays bidimensionais

--- scripts/Cap3-Arrays.js
+++ scripts/Cap3-Arrays.js
@@ -27,12 +27,54 @@
 
 //   console.log('number com o metodo shift: ', numbers)
 
-numbers = [-4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+// numbers = [-4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-// numbers.splice(0,2)
+// // numbers.splice(0,2)
 
-// console.log('Antes de add',numbers)
+// // console.log('Antes de add',numbers)
 
-numbers.splice(0,0,-6,-5)
+// numbers.splice(0,0,-6,-5)
 
-console.log('Dps de add:', numbers)
+// console.log('Dps de add:', numbers)
+
+
+
+
+// let avarageTemp=[]
+// avarageTemp[0] = [71,72,73,75]
+// avarageTemp[1] = [63,25,44,12]
+
+// Array.prototype.printMatrix = function(myMatrix){
+//     for (let i = 0; i < this.length; i++) {
+//         for (let j = 0; j < this[i].length; j++){
+//             console.log(this[i][j])
+
+//         }
+//         return this
+//     }
+    
+// }
+
+// avarageTemp = avarageTemp.printMatrix(this)
+// console.log(avarageTemp)
+
+
+let averageTemp = [];
+
+// same as
+averageTemp[0] = [72, 75, 79, 79, 81, 81];
+averageTemp[1] = [81, 79, 75, 75, 73, 73];
+
+function printMatrix(myMatrix) {
+  for (let i = 0; i < myMatrix.length; i++) {
+    for (let j = 0; j < myMatrix[i].length; j++) {
+      console.table(myMatrix[i][j]);
+    }
+  }
+}
+
+ printMatrix(averageTemp);
+console.table( averageTemp);
+
+document.write( averageTemp)
+
