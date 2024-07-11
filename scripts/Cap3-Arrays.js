8c7c1a2ac2fd5c29c88a7b72a350564143adc1fe
João Pedro <joao.pedro@dtidigital.com.br>
2024-07-11 18:04:59 -0300

    arrays bidimensionais e multidimensionais

--- scripts/Cap3-Arrays.js
+++ scripts/Cap3-Arrays.js
@@ -1,17 +1,16 @@
-
 // numbers = [-4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 // Array.prototype.reIndex = function(myArray) {
 //     const newArray = [];
 //     for(let i = 0; i < myArray.length; i++ ) {
 //       if (myArray[i] !== undefined) {
 //         console.log('ReIndex',myArray[i])
-        
+
 //         newArray.push(myArray[i]);
 //       }
 //     }
 //     console.log(myArray)
 //     return newArray;
-//   }  
+//   }
 // // remove first position manually and reIndex
 // Array.prototype.removeFirstPosition = function() {
 //     for (let i = 0; i < this.length; i++) {
@@ -19,7 +18,7 @@
 //     }
 //     return this.reIndex(this);
 //   };
-  
+
 //   numbers = numbers.removeFirstPosition();
 //   console.log('Removed first with removeFirstPosition + reIndex', numbers);
 
@@ -37,9 +36,6 @@
 
 // console.log('Dps de add:', numbers)
 
-
-
-
 // let avarageTemp=[]
 // avarageTemp[0] = [71,72,73,75]
 // avarageTemp[1] = [63,25,44,12]
@@ -52,29 +48,45 @@
 //         }
 //         return this
 //     }
-    
+
 // }
 
 // avarageTemp = avarageTemp.printMatrix(this)
 // console.log(avarageTemp)
 
+// let averageTemp = [];
 
-let averageTemp = [];
+// // same as
+// averageTemp[0] = [72, 75, 79, 79, 81, 81];
+// averageTemp[1] = [81, 79, 75, 75, 73, 73];
 
-// same as
-averageTemp[0] = [72, 75, 79, 79, 81, 81];
-averageTemp[1] = [81, 79, 75, 75, 73, 73];
+// function printMatrix(myMatrix) {
+//   for (let i = 0; i < myMatrix.length; i++) {
+//     for (let j = 0; j < myMatrix[i].length; j++) {
+//       console.table(myMatrix[i][j]);
+//     }
+//   }
+// }
 
-function printMatrix(myMatrix) {
-  for (let i = 0; i < myMatrix.length; i++) {
-    for (let j = 0; j < myMatrix[i].length; j++) {
-      console.table(myMatrix[i][j]);
+//  printMatrix(averageTemp);
+// console.table( averageTemp);
+
+// document.write( averageTemp)
+
+const matrix3x3 = [];
+for (let i = 0; i < 3; i++) {
+  matrix3x3[i] = [];
+
+  for (let j = 0; j < 3; j++) {
+    matrix3x3[i][j] = [];
+    for (let z = 0; z < 3; z++) {
+      matrix3x3[i][j][z] = i + j + z;
     }
   }
 }
-
- printMatrix(averageTemp);
-console.table( averageTemp);
-
-document.write( averageTemp)
-
+console.log(matrix3x3[0][0][1]);
+for (let i = 0; i < matrix3x3.length; i++) {
+  for (let j = 0; j < matrix3x3[i].length; j++) {
+    for (let z = 0; z < matrix3x3[i][j].length; z++) {}
+  }
+}
