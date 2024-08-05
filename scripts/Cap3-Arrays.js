ff06c031c737c401744a858fe6d54f66d3e1cd90
Joao Pedro <joao.fpsilva98@gmail.com>
2024-08-05 20:35:48 -0300

    to string and join methods

--- scripts/Cap3-Arrays.js
+++ scripts/Cap3-Arrays.js
@@ -199,5 +199,10 @@ for (const n of iterator) {
 
 let numbers = Array.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
 
-console.log(numbers.includes(15))
-console.log(numbers.includes(20))
+numbers.toString()
+
+console.log(numbers.toString())
+
+console.log(numbers.join('-'))
+
+
