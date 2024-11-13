633b55657a3964889f2040f458ee636b94d9400b
Joao Pedro <joao.fpsilva98@gmail.com>
2024-11-13 17:28:21 -0300

    binary converter

--- scripts/Cap4-Pilhas.js
+++ scripts/Cap4-Pilhas.js
@@ -1,98 +1,111 @@
-// class Stack {
-//     constructor () {
-//         this.items = []
+// // class Stack {
+// //     constructor () {
+// //         this.items = []
 
-//     }
-//     isEmpty() {
-//         return  this.items.length === 0;
-//         }
+// //     }
+// //     isEmpty() {
+// //         return  this.items.length === 0;
+// //         }
 
-//     push(index){
-//         return this.items.push(index)
-//     }
-//     pop(){
-//         return this.items.pop()
-//     }
-//     peek(){
-//         return this.items[this.items.length -1]
-//     }
-//     size() {
-//         return this.items.length
-//     }
+// //     push(index){
+// //         return this.items.push(index)
+// //     }
+// //     pop(){
+// //         return this.items.pop()
+// //     }
+// //     peek(){
+// //         return this.items[this.items.length -1]
+// //     }
+// //     size() {
+// //         return this.items.length
+// //     }
 
-//     clear(){
-//         return this.items = []
-//     }
-// }
-// const stack = new Stack()
-// console.log(stack.isEmpty())
+// //     clear(){
+// //         return this.items = []
+// //     }
+// // }
+// // const stack = new Stack()
+// // console.log(stack.isEmpty())
 
-// stack.push(1)
-// stack.push(2)
-// stack.push(3)
-// stack.push(4)
-// stack.push(5)
+// // stack.push(1)
+// // stack.push(2)
+// // stack.push(3)
+// // stack.push(4)
+// // stack.push(5)
 
-// stack.pop()
+// // stack.pop()
 
-// console.log(stack)
+// // console.log(stack)
 
-// console.log('Ultimo elemento:',stack.peek())
+// // console.log('Ultimo elemento:',stack.peek())
 
-// console.log('Numero de elementos contidos na pilha:',stack.size())
+// // console.log('Numero de elementos contidos na pilha:',stack.size())
 
-// // stack.clear()
+// // // stack.clear()
 
-// // console.log('Array dps do clear', stack)
+// // // console.log('Array dps do clear', stack)
 
 
 class Stack {
-    constructor () {
-        this.count = 0
-        this.items = {}
-
+    constructor() {
+        this.items = [];
     }
     isEmpty() {
-        return  this.items.lenght === 0
+        return  this.items.length === 0
         }
 
-    push(element){
-        this.items[this.count] = element
-        this.count ++
-    }
-    pop(){
-         if(this.isEmpty()){
-            return undefined
-         }
-         this.count--
-         const result = this.items[this.count]
-         delete this.items[this.count]
-
-         return result
-    }
-    peek(){
-        return this.items[this.count -1]
-    }
-    size() {
-        return this.count
+        push(element) {
+            this.items.push(element);
+        }
+    pop() {
+        if (this.isEmpty()) {
+            return undefined;
+        }
+        return this.items.pop();
     }
 
-    clear(){
-        this.items = {}
-        this.count = 0
-    }
+    
+    
 }
-const stack = new Stack()
-stack.push(5)
-stack.push(8)
-stack.push(3)
-stack.push(2)
-stack.push(1)
+
+// Função para converter decimal para binário
+function decimalToBinary(decNumber) {
+    const ramStack = new Stack();
+    let number = decNumber;
+    let rem;
+    let binaryString = '';
+    
+    // Loop para dividir o número por 2 e armazenar o resto na pilha
+    while (number > 0) {
+        rem = Math.floor(number % 2);
+        ramStack.push(rem);
+        number = Math.floor(number / 2);
+    }
+    
+    // Loop para construir a string binária a partir dos restos na pilha
+    while (!ramStack.isEmpty()) {
+        binaryString += ramStack.pop().toString();
+    }
+    
+    return binaryString;
+}
 
 
 
 
-stack.pop()
-console.log(stack.clear())
-console.log('Pilha apos o metodo clear()',stack)
-console.log('Is Empty?',stack.isEmpty())
+console.log(decimalToBinary(10)); // Saída esperada: "1010"
+
+
+// stack.push(5)
+// stack.push(8)
+// stack.push(3)
+// stack.push(2)
+// stack.push(1)
+
+
+
+
+// stack.pop()
+// console.log(stack.clear())
+// console.log('Pilha apos o metodo clear()',stack)
+// console.log('Is Empty?',stack.isEmpty())
