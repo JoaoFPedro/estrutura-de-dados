9a34290aebd9fb4299a5cc2ddad2cfd73a1b36b8
Joao Pedro <joao.fpsilva98@gmail.com>
2025-01-14 08:07:00 -0300

    insert method

--- scripts/Cap6-ListasLigadas/Cap6-ListasLigadas.js
+++ scripts/Cap6-ListasLigadas/Cap6-ListasLigadas.js
@@ -124,4 +124,37 @@ class DoublyLinkedList extends LinkedList {
     }
     this.count++;
   }
+  insert(element, index) {
+    if (index >= 0 && index <= this.count) {
+      const node = new DoublyNode(element);
+      let current = this.head;
+      if (index === 0) {
+        if (this.head == null) {
+          // NEW
+          this.head = node;
+          this.tail = node; // NEW
+        } else {
+          node.next = this.head;
+          this.head.prev = node; // NEW
+          this.head = node;
+        }
+      } else if (index === this.count) {
+        // last item NEW
+        current = this.tail;
+        current.next = node;
+        node.prev = current;
+        this.tail = node;
+      } else {
+        const previous = this.getElementAt(index - 1);
+        current = previous.next;
+        node.next = current;
+        previous.next = node;
+        current.prev = node; // NEW
+        node.prev = previous; // NEW
+      }
+      this.count++;
+      return true;
+    }
+    return false;
+  }
 }
