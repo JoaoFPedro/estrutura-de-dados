94d6d13ec44a006874f39072a129434869be3b98
Joao Pedro <joao.fpsilva98@gmail.com>
2025-01-21 18:11:47 -0300

    another way to use hasElement

--- scripts/Cap 7-Conjuntos/conjuntos.js
+++ scripts/Cap 7-Conjuntos/conjuntos.js
@@ -3,7 +3,8 @@ class Set {
     this.items = {};
   }
   hasElement(element) {
-    return element in this.items;
+    return element in this.items; // Aqui verifica se a cadeia de objetos tem a propriedade
+    //  return Object.prototype.hasOwnProperty.call(this.items, element); Aqui verifica se o objeto tem a propriedade especificada
   }
 }
 const conj = new Set();
