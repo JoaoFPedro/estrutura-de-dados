42471f21557acbdae3bf92a049be859dbb7d9f79
Joao Pedro <joao.fpsilva98@gmail.com>
2025-01-21 18:16:59 -0300

    addElement method

--- scripts/Cap 7-Conjuntos/conjuntos.js
+++ scripts/Cap 7-Conjuntos/conjuntos.js
@@ -6,6 +6,17 @@ class Set {
     return element in this.items; // Aqui verifica se a cadeia de objetos tem a propriedade
     //  return Object.prototype.hasOwnProperty.call(this.items, element); Aqui verifica se o objeto tem a propriedade especificada
   }
+  addElement(element) {
+    if (!this.hasElement(element)) {
+      this.items[element] = element;
+      return true;
+    }
+    return false;
+  }
 }
 const conj = new Set();
 console.log(conj.hasElement());
+conj.addElement(2);
+console.log(conj);
+
+conj.addElement(4);
