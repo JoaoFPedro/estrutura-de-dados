584ed75bce88c24aeecf44de7509578d9fe39f73
Joao Pedro <joao.fpsilva98@gmail.com>
2025-01-27 20:48:27 -0300

    union method

--- scripts/Cap 7-Conjuntos/conjuntos.js
+++ scripts/Cap 7-Conjuntos/conjuntos.js
@@ -29,17 +29,38 @@ class Set {
   value() {
     return Object.values(this.items);
   }
+  union(otherSet) {
+    const unionSet = new Set();
+    this.values().forEach((value) => unionSet.add(value));
+    otherSet.values().forEach((value) => unionSet.add(value));
+    return unionSet;
+  }
 }
-const conj = new Set();
-console.log(conj.hasElement());
-conj.addElement(2);
-console.log(conj);
+// const conj = new Set();
+// console.log(conj.hasElement());
+// conj.addElement(2);
+// console.log(conj);
 
-conj.addElement(4);
-console.log(conj.hasElement(2));
+// conj.addElement(4);
+// console.log(conj.hasElement(2));
 // conj.deleteElement(2);
 
 // console.log("Alterado", conj);
 
 // conj.clear();
 // console.log("After Clear method", conj);
+
+const setA = new Set();
+setA.addElement(1);
+setA.addElement(2);
+setA.addElement(3);
+
+const setB = new Set();
+setB.addElement(3);
+setB.addElement(4);
+setB.addElement(5);
+setB.addElement(6);
+
+const unionAB = setA.union(setB);
+
+console.log("UNIÃO**", unionAB);
