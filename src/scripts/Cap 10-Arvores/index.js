cd48d8cd3b7368ca1b91df8961ed946525e229ac
Joao Pedro <joao.fpsilva98@gmail.com>
2025-03-15 19:31:13 -0300

    insert and insertNode method

--- src/scripts/Cap 10-Arvores/index.js
+++ src/scripts/Cap 10-Arvores/index.js
@@ -5,3 +5,43 @@ class node {
     this.right = null;
   }
 }
+function defaultCompare(a, b) {
+  if (a === b) {
+    return Compare.EQUALS;
+  }
+  return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN;
+}
+const Compare = {
+  LESS_THAN: -1,
+  BIGGER_THAN: 1,
+  EQUALS: 0,
+};
+
+class BinarySearchTree {
+  constructor(compareFn = defaultCompare) {
+    this.compareFn = compareFn;
+    this.root = null;
+  }
+  insert(key) {
+    if (this.root === null) {
+      this.root = new Node(key);
+    } else {
+      this.insertNode(this.root, key);
+    }
+  }
+  insertNode(node, key) {
+    if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+      if (node.left === null) {
+        node.left = new Node(key);
+      } else {
+        this.insertNode(node.left, key);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = new Node(key);
+      } else {
+        this.insertNode(node.right, key);
+      }
+    }
+  }
+}
