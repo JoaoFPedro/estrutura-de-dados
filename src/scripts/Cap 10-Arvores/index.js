fab5f5dbcf6fd859e8c93fe733419be0bb19db28
Joao Pedro <joao.fpsilva98@gmail.com>
2025-03-18 18:56:45 -0300

    search method

--- src/scripts/Cap 10-Arvores/index.js
+++ src/scripts/Cap 10-Arvores/index.js
@@ -93,6 +93,21 @@ class BinarySearchTree {
     }
     return current;
   }
+  search(key) {
+    return this.searchNode(this.root, key);
+  }
+  searchNode(node, key) {
+    if (node == null) {
+      return false;
+    }
+    if (this.compareFn(key, node.key) == Compare.LESS_THAN) {
+      return this.searchNode(node.left, key);
+    } else if (this.compareFn(key, node.key) === Compare.BIGGER_THAN) {
+      return this.searchNode(node.right, key);
+    } else {
+      return true;
+    }
+  }
 }
 const tree = new BinarySearchTree();
 tree.insert(11);
