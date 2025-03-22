7954c245889a7c46fb5db92e6836ea6eb072e2cf
Joao Pedro <joao.fpsilva98@gmail.com>
2025-03-22 10:51:24 -0300

    red black tree

--- src/scripts/Cap 10-Arvores/index.js
+++ src/scripts/Cap 10-Arvores/index.js
@@ -163,104 +163,282 @@ class BinarySearchTree {
 
 //AVL tree é uma arvore que se auto regula, uma tree que tenta se balanceear sempre que um nó é add, evitando ter + de 1 de diferença em qualquer nivel
 
-const BalanceFactor = {
-  UNBALANCED_RIGHT: 1,
-  SLIGHTLY_UNBALANCED_RIGHT: 2,
-  BALANCED: 3,
-  SLIGHTLY_UNBALANCED_LEFT: 4,
-  UNBALANCED_LEFT: 5,
-};
-class AVLTree extends BinarySearchTree {
-  constructor(compareFn = defaultCompare) {
+// const BalanceFactor = {
+//   UNBALANCED_RIGHT: 1,
+//   SLIGHTLY_UNBALANCED_RIGHT: 2,
+//   BALANCED: 3,
+//   SLIGHTLY_UNBALANCED_LEFT: 4,
+//   UNBALANCED_LEFT: 5,
+// };
+// class AVLTree extends BinarySearchTree {
+//   constructor(compareFn = defaultCompare) {
+//     super(compareFn);
+//     this.compareFn = compareFn;
+//     this.root = null;
+//   }
+//   getNodeHeight(node) {
+//     if (node == null) {
+//       return -1;
+//     }
+//     return (
+//       Math.max(this.getNodeHeight(node.left), this.getNodeHeight(node.right)) +
+//       1
+//     );
+//   }
+//   getBalanceFactor(node) {
+//     const heightDifference =
+//       this.getNodeHeight(node.left) - this.getNodeHeight(node.right);
+//     switch (heightDifference) {
+//       case -2:
+//         return BalanceFactor.UNBALANCED_RIGHT;
+//       case -1:
+//         return BalanceFactor.SLIGHTLY_UNBALANCED_RIGHT;
+//       case 1:
+//         return BalanceFactor.SLIGHTLY_UNBALANCED_LEFT;
+//       case 2:
+//         return BalanceFactor.UNBALANCED_LEFT;
+//       default:
+//         return BalanceFactor.BALANCED;
+//     }
+//   }
+//   /**
+//    * @param node Node<T>
+//    */
+
+//   rotationLL(node) {
+//     const tmp = node.left;
+//     node.left = tmp.right;
+//     tmp.right = node;
+//     return tmp;
+//   }
+//   rotationRR(node) {
+//     const tmp = node.right;
+//     node.right = tmp.left;
+//     tmp.left = node;
+//     return tmp;
+//   }
+//   rotationRL(node) {
+//     node.right = this.rotationLL(node.right);
+//     return this.rotationRR(node);
+//   }
+//   rotationLR(node) {
+//     node.left = this.rotationRR(node.left);
+//     return this.rotationLL(node);
+//   }
+//   insert(key) {
+//     this.root = this.insertNode(this.root, key);
+//   }
+//   insertNode(node, key) {
+//     if (node == null) {
+//       return new Node(key);
+//     } else if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+//       node.left = this.insertNode(node.left, key);
+//     } else if (this.compareFn(key, node.key) === Compare.BIGGER_THAN) {
+//       node.right = this.insertNode(node.right, key);
+//     } else {
+//       return node; // duplicated key
+//     }
+//     // verify if tree is balanced
+//     const balanceFactor = this.getBalanceFactor(node);
+//     if (balanceFactor === BalanceFactor.UNBALANCED_LEFT) {
+//       if (this.compareFn(key, node.left.key) === Compare.LESS_THAN) {
+//         // Left left case
+//         node = this.rotationLL(node);
+//       } else {
+//         // Left right case
+//         return this.rotationLR(node);
+//       }
+//     }
+//     if (balanceFactor === BalanceFactor.UNBALANCED_RIGHT) {
+//       if (this.compareFn(key, node.right.key) === Compare.BIGGER_THAN) {
+//         // Right right case
+//         node = this.rotationRR(node);
+//       } else {
+//         // Right left case
+//         return this.rotationRL(node);
+//       }
+//     }
+//     return node;
+//   }
+//   removeNode(node, key) {
+//     node = super.removeNode(node, key);
+//   }
+// }
+
+//Black-red Tree
+export default class RedBlackTree<T> extends BinarySearchTree<T> {
+  protected root: RedBlackNode<T>;
+
+  constructor(protected compareFn: ICompareFunction<T> = defaultCompare) {
     super(compareFn);
-    this.compareFn = compareFn;
-    this.root = null;
   }
-  getNodeHeight(node) {
-    if (node == null) {
-      return -1;
-    }
-    return (
-      Math.max(this.getNodeHeight(node.left), this.getNodeHeight(node.right)) +
-      1
-    );
-  }
-  getBalanceFactor(node) {
-    const heightDifference =
-      this.getNodeHeight(node.left) - this.getNodeHeight(node.right);
-    switch (heightDifference) {
-      case -2:
-        return BalanceFactor.UNBALANCED_RIGHT;
-      case -1:
-        return BalanceFactor.SLIGHTLY_UNBALANCED_RIGHT;
-      case 1:
-        return BalanceFactor.SLIGHTLY_UNBALANCED_LEFT;
-      case 2:
-        return BalanceFactor.UNBALANCED_LEFT;
-      default:
-        return BalanceFactor.BALANCED;
-    }
-  }
+
   /**
+   * Left left case: rotate right
+   *
+   *       b                           a
+   *      / \                         / \
+   *     a   e -> rotationLL(b) ->   c   b
+   *    / \                             / \
+   *   c   d                           d   e
+   *
    * @param node Node<T>
    */
-
-  rotationLL(node) {
+  private rotationLL(node: RedBlackNode<T>) {
     const tmp = node.left;
     node.left = tmp.right;
+    if (tmp.right && tmp.right.key) {
+      tmp.right.parent = node;
+    }
+    tmp.parent = node.parent;
+    if (!node.parent) {
+      this.root = tmp;
+    } else {
+      if (node === node.parent.left) {
+        node.parent.left = tmp;
+      } else {
+        node.parent.right = tmp;
+      }
+    }
     tmp.right = node;
-    return tmp;
+    node.parent = tmp;
   }
-  rotationRR(node) {
+
+  /**
+   * Right right case: rotate left
+   *
+   *     a                              b
+   *    / \                            / \
+   *   c   b   -> rotationRR(a) ->    a   e
+   *      / \                        / \
+   *     d   e                      c   d
+   *
+   * @param node Node<T>
+   */
+  private rotationRR(node: RedBlackNode<T>) {
     const tmp = node.right;
     node.right = tmp.left;
-    tmp.left = node;
-    return tmp;
-  }
-  rotationRL(node) {
-    node.right = this.rotationLL(node.right);
-    return this.rotationRR(node);
-  }
-  rotationLR(node) {
-    node.left = this.rotationRR(node.left);
-    return this.rotationLL(node);
-  }
-  insert(key) {
-    this.root = this.insertNode(this.root, key);
-  }
-  insertNode(node, key) {
-    if (node == null) {
-      return new Node(key);
-    } else if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
-      node.left = this.insertNode(node.left, key);
-    } else if (this.compareFn(key, node.key) === Compare.BIGGER_THAN) {
-      node.right = this.insertNode(node.right, key);
+    if (tmp.left && tmp.left.key) {
+      tmp.left.parent = node;
+    }
+    tmp.parent = node.parent;
+    if (!node.parent) {
+      this.root = tmp;
     } else {
-      return node; // duplicated key
-    }
-    // verify if tree is balanced
-    const balanceFactor = this.getBalanceFactor(node);
-    if (balanceFactor === BalanceFactor.UNBALANCED_LEFT) {
-      if (this.compareFn(key, node.left.key) === Compare.LESS_THAN) {
-        // Left left case
-        node = this.rotationLL(node);
+      if (node === node.parent.left) {
+        node.parent.left = tmp;
       } else {
-        // Left right case
-        return this.rotationLR(node);
+        node.parent.right = tmp;
       }
     }
-    if (balanceFactor === BalanceFactor.UNBALANCED_RIGHT) {
-      if (this.compareFn(key, node.right.key) === Compare.BIGGER_THAN) {
-        // Right right case
-        node = this.rotationRR(node);
+    tmp.left = node;
+    node.parent = tmp;
+  }
+
+  insert(key: T) {
+    // special case: first key
+    if (this.root == null) {
+      this.root = new RedBlackNode(key);
+      this.root.color = Colors.BLACK;
+    } else {
+      const newNode = this.insertNode(this.root, key);
+      this.fixTreeProperties(newNode);
+    }
+  }
+
+  protected insertNode(node: RedBlackNode<T>, key: T): RedBlackNode<T> {
+    if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+      if (node.left == null) {
+        node.left = new RedBlackNode(key);
+        node.left.parent = node;
+        return node.left;
       } else {
-        // Right left case
-        return this.rotationRL(node);
+        return this.insertNode(node.left, key);
+      }
+    } else if (node.right == null) {
+      node.right = new RedBlackNode(key);
+      node.right.parent = node;
+      return node.right;
+    } else {
+      return this.insertNode(node.right, key);
+    }
+  }
+
+  private fixTreeProperties(node: RedBlackNode<T>) {
+    while (node && node.parent && node.parent.color === Colors.RED && node.color !== Colors.BLACK) {
+       let parent = node.parent;
+       const grandParent = parent.parent;
+
+      // case A
+      if (grandParent && grandParent.left === parent) {
+
+        const uncle = grandParent.right;
+
+        // case 1: uncle of node is also red - only recoloring
+        if (uncle && uncle.color === Colors.RED) {
+          grandParent.color = Colors.RED;
+          parent.color = Colors.BLACK;
+          uncle.color = Colors.BLACK;
+          node = grandParent;
+        } else {
+          // case 2: node is right child - left rotate
+          if (node === parent.right) {
+            this.rotationRR(parent);
+            node = parent;
+            parent = node.parent;
+          }
+
+          // case 3: node is left child - right rotate
+          this.rotationLL(grandParent);
+          // swap color
+          parent.color = Colors.BLACK;
+          grandParent.color = Colors.RED;
+          node = parent;
+        }
+
+      } else { // case B: parent is right child of grand parent
+
+        const uncle = grandParent.left;
+
+        // case 1: uncle is read - only recoloring
+        if (uncle && uncle.color === Colors.RED) {
+          grandParent.color = Colors.RED;
+          parent.color = Colors.BLACK;
+          uncle.color = Colors.BLACK;
+          node = grandParent;
+        } else {
+          // case 2: node is left child - left rotate
+          if (node === parent.left) {
+            this.rotationLL(parent);
+            node = parent;
+            parent = node.parent;
+          }
+
+           // case 3: node is right child - left rotate
+          this.rotationRR(grandParent);
+          // swap color
+          parent.color = Colors.BLACK;
+          grandParent.color = Colors.RED;
+          node = parent;
+        }
       }
     }
-    return node;
+    this.root.color = Colors.BLACK;
   }
-  removeNode(node, key) {
-    node = super.removeNode(node, key);
+
+  getRoot() {
+    return this.root;
   }
+
+  /* private flipColors(node: RedBlackNode<T>) {
+    node.left.flipColor();
+    node.right.flipColor();
+  }
+
+  private isRed(node: RedBlackNode<T>) {
+    if (!node) {
+      return false;
+    }
+    return node.isRed();
+  }*/
 }
