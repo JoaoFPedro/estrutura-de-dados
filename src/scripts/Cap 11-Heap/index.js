02cef361e243134c9b59ef16e4fc286424737745
Joao Pedro <joao.fpsilva98@gmail.com>
2025-03-24 18:43:13 -0300

    swap method

--- src/scripts/Cap 11-Heap/index.js
+++ src/scripts/Cap 11-Heap/index.js
@@ -4,7 +4,12 @@ function defaultCompare(a, b) {
   }
   return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN;
 }
-
+function swap(array, a, b) {
+  /* const temp = array[a];
+    array[a] = array[b];
+    array[b] = temp; */
+  [array[a], array[b]] = [array[b], array[a]];
+}
 export class Minheap {
   constructor(compareFn = defaultCompare) {
     this.compareFn = compareFn;
