92bf93631299e0e5c295429fceb93c66c265a7f3
Joao Pedro <joao.fpsilva98@gmail.com>
2025-03-24 18:33:45 -0300

    initialize heap class

---
+++ src/scripts/Cap 11-Heap/index.js
@@ -0,0 +1,25 @@
+function defaultCompare(a, b) {
+  if (a === b) {
+    return Compare.EQUALS;
+  }
+  return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN;
+}
+
+export class Minheap {
+  constructor(compareFn = defaultCompare) {
+    this.compareFn = compareFn;
+    this.heap = [];
+  }
+  getLeftIndex(index) {
+    return 2 * index + 1;
+  }
+  getRightIndex(index) {
+    return 2 * index + 2;
+  }
+  getParentIndex(index) {
+    if (index === 0) {
+      return undefined;
+    }
+    return Math.floor((index - 1) / 2);
+  }
+}
