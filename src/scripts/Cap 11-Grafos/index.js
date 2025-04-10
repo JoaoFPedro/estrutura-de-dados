0f8a3676334fb04817f408a8396db99a29601d11
Joao Pedro <joao.fpsilva98@gmail.com>
2025-04-10 18:40:41 -0300

    grafos methods

--- src/scripts/Cap 11-Grafos/index.js
+++ src/scripts/Cap 11-Grafos/index.js
@@ -75,4 +75,34 @@ class Grap {
       this.adjList.set(v, []); //Cria uma nova entrada onde a chave é o vertice V e o valor é uma lista vazia que receberá os vertices conectados
     }
   }
+  addEdge(a, b) {
+    if (!this.adjList.get(a)) {
+      this.addVertex(a);
+    }
+    if (!this.adjList.get(b)) {
+      this.addVertex(b);
+    }
+    this.adjList.get(a).push(b);
+    if (this.isDirected !== true) {
+      this.adjList.get(b).push(a);
+    }
+  }
+  getVertices() {
+    return this.vertices;
+  }
+  getAdjList() {
+    return this.adjList;
+  }
+  toString() {
+    let s = "";
+    for (let i = 0; i < this.vertices.length; i++) {
+      s += `${this.vertices[i]} -> `;
+      const neighbors = this.adjList.get(this.vertices[i]);
+      for (let j = 0; j < neighbors.length; j++) {
+        s += `${neighbors[j]} `;
+      }
+      s += "\n";
+    }
+    return s;
+  }
 }
